c7018b8eec974934847a0daa10de7b3e9d323f53
Om Tomar <123073338+omsenpaiii@users.noreply.github.com>
2023-10-22 20:31:12 +0530

    Update Task5.2C GUI.py

--- Task5.2C GUI.py
+++ Task5.2C GUI.py
@@ -1,18 +1,25 @@
+# Import the required modules from tkinter and gpiozero
 from tkinter import *
 import tkinter.font
 from gpiozero import LED
 import RPi.GPIO
 
+# Set the GPIO mode to BCM
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 
+# Initialize LED objects for red, green, and yellow lights
 red = LED(14)
 green = LED(15)
 yellow = LED(18)
 
+# Create the main Tkinter window
 win = Tk()
 win.title("Traffic Light Toggler")
+
+# Define a custom font for the user interface
 myFont = tkinter.font.Font(family='Helvetica', size=12, weight="bold")
 
+# Function to toggle the state of an LED
 def toggle_led(led, other_leds):
     if led.is_lit:
         led.off()
@@ -21,6 +28,7 @@ def toggle_led(led, other_leds):
         for ol in other_leds:
             ol.off()
 
+# Functions to control the red, green, and yellow LEDs
 def RedLed():
     toggle_led(red, [green, yellow])
 
@@ -30,12 +38,15 @@ def GreenLed():
 def YellowLed():
     toggle_led(yellow, [red, green])
 
+# Function to clean up GPIO and close the window
 def close():
     RPi.GPIO.cleanup()
     win.destroy()
 
+# Create an IntVar to hold the selected LED
 selected_led = IntVar()
 
+# Create radio buttons for the red, green, and yellow LEDs
 redButton = Radiobutton(win, text='Red LED', font=myFont, variable=selected_led, value=1, command=RedLed, bg='red')
 redButton.grid(row=0, column=1)
 
@@ -45,8 +56,12 @@ greenButton.grid(row=1, column=1)
 yellowButton = Radiobutton(win, text='Yellow LED', font=myFont, variable=selected_led, value=3, command=YellowLed, bg='yellow')
 yellowButton.grid(row=2, column=1)
 
+# Create an exit button to clean up and close the application
 exitButton = Button(win, text='Exit', font=myFont, command=close, bg='grey', height=1, width=6)
 exitButton.grid(row=3, column=1)
 
+# Handle window close event to ensure cleanup
 win.protocol("WM_DELETE_WINDOW", close)
+
+# Start the Tkinter event loop
 win.mainloop()
